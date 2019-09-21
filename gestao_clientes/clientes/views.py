e2ec27662beb65a4714082cb1d209527db0ed649
Tiago Yamamoto Ribeiro <japa@rede5.com.br>
2019-09-21 15:46:36 -0300

    34. Personalizando os formulários com Bootstrap

--- gestao_clientes/clientes/views.py
+++ gestao_clientes/clientes/views.py
@@ -15,7 +15,7 @@ def persons_new(request):
     form = PersonForm(request.POST or None, request.FILES or None)
     if form.is_valid():
         form.save()
-    return redirect('person_list')
+        return redirect('person_list')
     return render(request, 'person_form.html', {'form': form})
 
 
